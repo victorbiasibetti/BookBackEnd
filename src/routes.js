f8723591c0dbf00d04301b8e49dd01cf781e4df4
Victor Biasibetti <victorbiasibetti7@gmail.com>
2020-01-19 23:27:04 -0300

    add route test

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const BookController = require('./app/controllers/BookController')
 
 const routes = new Router();
 
+routes.get('/hello', async (req, res) => res.send({message: "hello world"}))
 routes.post('/', async (req, res) => BookController.store(req, res))
 routes.get('/', async (req, res) => BookController.list(req, res))
 routes.get('/:id', async (req, res) => BookController.get(req, res))
